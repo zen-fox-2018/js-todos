12a5214d20c2f96ee89200d64e33facbc3b560b1
Januarsmad <januarsmad@gmail.com>
2018-12-05 16:32:02 +0700

    first commit

--- todo.js
+++ todo.js
@@ -1,1 +1,37 @@
+const Controller = require('./controller/Controller.js')
 
+const argv = process.argv.slice(2)
+
+switch (argv[0]) {
+    case 'help':
+        Controller.help()        
+        break;
+
+    case 'list':
+        Controller.list()
+        break;
+    
+    case 'add':
+        Controller.add(argv[1])
+        break;
+
+    case 'findById':
+        Controller.findById(argv[1])
+        break;
+
+    case 'delete':
+        Controller.delete(argv[1])
+        break;
+    
+    case 'complete':
+        Controller.complete(argv[1])
+        break;
+       
+    case 'uncomplete':
+        Controller.uncomplete(argv[1])
+        break;
+        
+    default:
+        Controller.help()
+        break;
+}
